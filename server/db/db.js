75a1b5c297b700e2d8943b9a4917e8642845069d
jakelw96 <jake.lw96@gmail.com>
2022-01-03 15:45:50 -0700

    Initial set-up of database and project files

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,13 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+require('dotenv').config();
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  logging: false,
+  host: 'localhost',
+  dialect:'postgres',
+  port: '5432',
+  protocol: 'postgres'
 });
 
 module.exports = db;
