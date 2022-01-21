300b40c64eeb5579849b2aeaf1fdb0a4fab50494
jakelw96 <jake.lw96@gmail.com>
2022-01-20 23:58:21 -0700

    Updated model associations, conversation model, and added a new junction model userConversation

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,22 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const userConversation = require("./userConversation");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.belongsToMany(Conversation, {
+  through: userConversation,
+});
+Conversation.belongsToMany(User, {
+  through: userConversation,
+});
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  userConversation,
 };
